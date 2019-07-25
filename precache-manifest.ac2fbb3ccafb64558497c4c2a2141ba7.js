c0ef098c79fad3ed0811874a4e5cab20c11a3d0d
Viola Hu <lovelyviolaat@hotmail.com>
2019-07-25 15:29:26 +1000

    Updates

---
+++ precache-manifest.ac2fbb3ccafb64558497c4c2a2141ba7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b886321d918ee0942641e09c73a7fa8",
+    "url": "/proj3_react_frontend/index.html"
+  },
+  {
+    "revision": "d9cb7fcc4d0748845a14",
+    "url": "/proj3_react_frontend/static/css/main.984101de.chunk.css"
+  },
+  {
+    "revision": "ee52dc0e784d0a8a1868",
+    "url": "/proj3_react_frontend/static/js/2.b5cc8665.chunk.js"
+  },
+  {
+    "revision": "d9cb7fcc4d0748845a14",
+    "url": "/proj3_react_frontend/static/js/main.d841a3ba.chunk.js"
+  },
+  {
+    "revision": "bae058aa5bcbbbf9098f",
+    "url": "/proj3_react_frontend/static/js/runtime~main.9aa6579d.js"
+  }
+]);
