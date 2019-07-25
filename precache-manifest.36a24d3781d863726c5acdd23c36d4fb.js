7565864255ce0e167809d1495a4fafb5c231debc
Viola Hu <lovelyviolaat@hotmail.com>
2019-07-25 17:05:47 +1000

    Updates

--- precache-manifest.36a24d3781d863726c5acdd23c36d4fb.js
+++ precache-manifest.36a24d3781d863726c5acdd23c36d4fb.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1173a1e58568dfcd23a6e55db3733734",
+    "revision": "5cd91a61075a6e981db81ff07a5fdd3f",
     "url": "/proj3_react_frontend/index.html"
   },
   {
-    "revision": "12ae362da1994d79cbe4",
-    "url": "/proj3_react_frontend/static/css/main.984101de.chunk.css"
+    "revision": "61ec8376f6238fbe6371",
+    "url": "/proj3_react_frontend/static/css/main.c2adc980.chunk.css"
   },
   {
     "revision": "ee52dc0e784d0a8a1868",
     "url": "/proj3_react_frontend/static/js/2.b5cc8665.chunk.js"
   },
   {
-    "revision": "12ae362da1994d79cbe4",
+    "revision": "61ec8376f6238fbe6371",
     "url": "/proj3_react_frontend/static/js/main.9bfa1ed8.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/proj3_react_frontend/precache-manifest.855ce2a4c003c8a903889f885ca0ffe4.js"
+  "/proj3_react_frontend/precache-manifest.36a24d3781d863726c5acdd23c36d4fb.js"
 );
 
 self.addEventListener('message', (event) => {
