d4db93f88c3fd985cd7ffac9bcef197a15553472
Viola Hu <lovelyviolaat@hotmail.com>
2019-08-08 16:54:02 +1000

    changed url from local host to heroku

--- src/lib/url.js
+++ src/lib/url.js
@@ -1,8 +1,8 @@
 // when local test!
-const URL = 'http://localhost:3000';
+// const URL = 'http://localhost:3000';
 
 // when deployment, change to below
-// const URL = "https://toyshoppingsite.herokuapp.com";
+const URL = "https://toyshoppingsite.herokuapp.com";
 
 export default{
   URL
